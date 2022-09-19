5950a0aab2bdeb159f67b56e52ace10430d5383b
Andres Dominguez <andresdomlez@gmail.com>
2022-09-19 18:07:01 +0100

    Settings + sing in and up logic

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AuhtModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [
+    AuhtModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+  ],
+})
+export class AppModule {}
